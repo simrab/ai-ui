b781dc8b76124242ee56044b35059c41666aa2d3
Simrablab <msiardi7@hotmail.com>
2025-10-12 13:42:27 +0200

    fix(node): add nvmrc and fix post config

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,10 +1,5 @@
-// postcss.config.mjs
-// We need to import the plugin this way for Vitest to work correctly.
-import tailwindcssPostcss from "@tailwindcss/postcss";
-
 const config = {
-  plugins: [tailwindcssPostcss],
+  plugins: ["@tailwindcss/postcss"],
 };
 
 export default config;
-
